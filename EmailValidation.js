969925602c2fcb9877993fc31de65e2f0621ca8a
Akash B G <akashbpura@gmail.com>
2022-02-01 22:54:00 +0530

    Modified to validate for given test cases

--- EmailValidation.js
+++ EmailValidation.js
@@ -1,5 +1,5 @@
 let email = prompt('Enter the Email-id to be validated: ')
-const emailRegex = RegExp('^[a-zA-Z0-9][- . + _]?[a-zA-Z0-9]*[@]{1}[bridgelabz][.]{1}[co.][a-z]*$')
+const emailRegex = RegExp('^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$')
 function validationOfEmail(email) {
     if (emailRegex.test(email)) {
         console.log('Entered Email is valid!!')
@@ -14,4 +14,129 @@ try {
 }
 catch (e) {
     console.error(e);
+}
+//Test Cases ----->
+let testEmail = 'kira@bridgelabz.co.in'
+
+console.log('Valid Email Tests: ')
+validateEmail(testEmail)
+
+testEmail = 'abc@gmail.com.com'
+validateEmail(testEmail)
+
+testEmail = 'abc@yahoo.com'
+validateEmail(testEmail)
+
+testEmail = 'abc@1.com'
+validateEmail(testEmail)
+
+testEmail = 'abc-100@yahoo.com'
+validateEmail(testEmail)
+
+testEmail = 'abc.100@yahoo.com'
+validateEmail(testEmail)
+
+testEmail = 'abc111@abc.com'
+validateEmail(testEmail)
+
+testEmail = 'abc-100@abc.net'
+validateEmail(testEmail)
+
+testEmail = 'abc.100@abc.com.au'
+validateEmail(testEmail)
+
+testEmail = 'abc+100@gmail.com'
+validateEmail(testEmail)
+
+console.log('Invalid Email Tests: ')
+try {
+    testEmail = '.abc@abc.com'
+validateEmail(testEmail)
+}
+catch (e) {
+    console.error(e)
+}
+try {
+    testEmail = 'abc'
+validateEmail(testEmail)
+}
+catch (e) {
+    console.error(e)
+}
+try {
+    testEmail = 'abc@.com.my'
+validateEmail(testEmail)
+}
+catch (e) {
+    console.error(e)
+}
+try {
+    testEmail = 'abc@abc@gmail.com'
+validateEmail(testEmail)
+}
+catch (e) {
+    console.error(e)
+}
+try {
+    testEmail = 'abc()*@gmail.com'
+validateEmail(testEmail)
+}
+catch (e) {
+    console.error(e)
+}
+try {
+    testEmail = 'abc..2002@gmail.com'
+validateEmail(testEmail)
+}
+catch (e) {
+    console.error(e)
+}
+try {
+    testEmail = 'abc.@gmail.com'
+validateEmail(testEmail)
+}
+catch (e) {
+    console.error(e)
+}
+try {
+    testEmail = 'abc123@.com'
+validateEmail(testEmail)
+}
+catch (e) {
+    console.error(e)
+}
+try {
+    testEmail = 'abc123@.com.com'
+validateEmail(testEmail)
+}
+catch (e) {
+    console.error(e)
+}
+try {
+    testEmail = 'abc123@gmail.a'
+validateEmail(testEmail)
+}
+catch (e) {
+    console.error(e)
+}
+try {
+    testEmail = 'abc@%*.com'
+validateEmail(testEmail)
+}
+catch (e) {
+    console.error(e)
+}
+try {
+    testEmail = 'abc@gmail.com.1a'
+validateEmail(testEmail)
+}
+catch (e) {
+    console.error(e)
+}
+try {
+testEmail = 'abc@gmail.com.aa.au'
+validateEmail(testEmail)
+}
+catch (e) {
+    console.error(e)
 }
